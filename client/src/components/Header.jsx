1672951d337a83de6711e05015d22f176810124a
Yasan Ransika <yransika20010216@gmail.com>
2024-08-21 00:18:50 +0530

    create sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,25 +1,23 @@
-import { Button,Navbar,TextInput } from 'flowbite-react';
+import { Button, Navbar, TextInput } from 'flowbite-react';
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
-
-
-
 
 export default function Header() {
   const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2 flex justify-between items-center px-4'>
       <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-        <span className='px-2 py-1 bg-gradient-to-r from-yellow-500 via-light yellow-500 to-yellow-500 rounded-lg text-white'>BOC's Blog</span>
+        <span className='px-2 py-1 bg-gradient-to-r from-yellow-500 via-yellow-500 to-yellow-500 rounded-lg text-white'>BOC's Blog</span>
       </Link>
       <form className=''>
         <TextInput
           type='text'
           placeholder='Search...'
           rightIcon={AiOutlineSearch}
-          className='hidden lg:inline'/>
+          className='hidden lg:inline'
+        />
       </form>
       <Button className='w-12 h-10 lg:hidden' color='gray' pill>
         <AiOutlineSearch />
@@ -31,19 +29,19 @@ export default function Header() {
         <Link to='/sign-in' className='button-link'>
           <Button gradientDuoTone='purpleToBlue'>Sign In</Button>
         </Link>
-        <Navbar.Toggle/>
+        <Navbar.Toggle />
       </div>
-      <Navbar.Collapse>
-          <Navbar.Link active={path === '/'} as={'div'}>
-            <Link to='/'> Home </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === '/about'} as={'div'}>
-            <Link to='/about'> About </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === '/projects'} as={'div'}>
-            <Link to='/projects'>Projects </Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+      <Navbar.Collapse className="justify-center">
+        <Navbar.Link as={'div'} className={path === '/' ? 'active' : ''}>
+          <Link to='/'>Home</Link>
+        </Navbar.Link>
+        <Navbar.Link as={'div'} className={path === '/about' ? 'active' : ''}>
+          <Link to='/about'>About</Link>
+        </Navbar.Link>
+        <Navbar.Link as={'div'} className={path === '/projects' ? 'active' : ''}>
+          <Link to='/projects'>Projects</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,51 @@
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+
 
 export default function Signup() {
   return (
-    <div>Signup</div>
-  )
+    <div className='min-h-screen flex justify-center items-start'>
+      <div className='flex p-3 max-w-3xl flex-col items-center mt-20 md:flex-row gap-8'>
+{/* left */}
+        <div className='flex-1 text-center'>
+          <Link to="/" className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-yellow-500 via-yellow-500 to-yellow-500 rounded-lg text-white'>
+              BOC Blog
+            </span>
+          </Link>
+          <p className='text-sm mt-5'>
+            This is a BOC Blog. You can sign up with your email and password or with Google.
+          </p>
+        </div>
+{/* right */}
+        <div className=''>
+          <form className='flex flex-col gap-4'>
+            <div>
+              <Label value='Your username' />
+              <TextInput type='text' placeholder='Username' id='username' />
+            </div>
+            <div>
+              <Label value='Your email' />
+              <TextInput type='email' placeholder='name@company.com' id='email' />
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput type='password' placeholder='Password' id='password' />
+            </div>
+            <Button
+              gradientDuoTone='purpleToPink'
+              type='submit'>
+                Sign Up
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account?</span>
+            <Link to='/sign-in' className='text-blue-500'>
+              Sign In
+            </Link>
+          </div> 
+      </div>
+     </div>
+    </div>
+  );
 }
