6ae124a4a13d60e5b0cb230d0bc380d3c85a888b
Yasan Ransika <yransika20010216@gmail.com>
2024-09-09 22:05:46 +0530

    add functionality to the sign up page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,8 +1,45 @@
 import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+//import OAuth from '../components/OAuth';
 
+// Sign-up page UI 
 
 export default function Signup() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null); // loading craser
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);  //duplicate key have show error msg
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/sign-in');
+      }
+    } catch (error){
+      setErrorMessage(error.message);  //error of client slide
+      setLoading(false);
+    }
+  }; 
   return (
     <div className='min-h-screen flex justify-center items-start'>
       <div className='flex p-3 max-w-3xl flex-col items-center mt-20 md:flex-row gap-8'>
@@ -18,24 +55,32 @@ export default function Signup() {
           </p>
         </div>
 {/* right */}
-        <div className=''>
-          <form className='flex flex-col gap-4'>
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your username' />
-              <TextInput type='text' placeholder='Username' id='username' />
+              <TextInput type='text' placeholder='Username' id='username' onChange={handleChange} />
             </div>
             <div>
               <Label value='Your email' />
-              <TextInput type='email' placeholder='name@company.com' id='email' />
+              <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange} />
             </div>
             <div>
               <Label value='Your password' />
-              <TextInput type='password' placeholder='Password' id='password' />
+              <TextInput type='password' placeholder='Password' id='password' onChange={handleChange} />
             </div>
             <Button
               gradientDuoTone='purpleToPink'
-              type='submit'>
-                Sign Up
+              type='submit'
+              disabled={loading}>
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign Up'
+              )}
             </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
@@ -44,6 +89,11 @@ export default function Signup() {
               Sign In
             </Link>
           </div> 
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
       </div>
      </div>
     </div>
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
-    hmr: {
-      overlay: false, // Optionally disable the Vite overlay for a better DX experience
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
     },
   },
-})
+  plugins: [react()],
+});
