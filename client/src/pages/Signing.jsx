ce3ee4b15c64f3c366dbcbb948b32acbd51eed0e
Yasan Ransika <yransika20010216@gmail.com>
2024-09-10 20:09:05 +0530

    Complete signing page functionality

--- client/src/pages/Signing.jsx
+++ client/src/pages/Signing.jsx
@@ -1,6 +1,97 @@
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+//import OAuth from '../components/OAuth';
 
-export default function Signing() {
+// Sign-Ip page UI 
+
+export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null); // loading craser
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if ( !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields.');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signing', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setErrorMessage(data.message);  //duplicate key have show error msg
+      }
+      setLoading(false);
+      if(res.ok) {
+        navigate('/');
+      }
+    } catch (error){
+      setErrorMessage(error.message);  //error of client slide
+      setLoading(false);
+    }
+  }; 
   return (
-    <div>Signing</div>
-  )
+    <div className='min-h-screen flex justify-center items-start'>
+      <div className='flex p-3 max-w-3xl flex-col items-center mt-20 md:flex-row gap-8'>
+{/* left */}
+        <div className='flex-1 text-center'>
+          <Link to="/" className='font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-yellow-500 via-yellow-500 to-yellow-500 rounded-lg text-white'>
+              BOC Blog
+            </span>
+          </Link>
+          <p className='text-sm mt-5'>
+            This is a BOC Blog. You can sign In with your email and password or with Google.
+          </p>
+        </div>
+{/* right */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label value='Your email' />
+              <TextInput type='email' placeholder='name@company.com' id='email' onChange={handleChange} />
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput type='password' placeholder='************' id='password' onChange={handleChange} />
+            </div>
+            <Button
+              gradientDuoTone='purpleToPink'
+              type='submit'
+              disabled={loading}>
+              {loading ? (
+                <>
+                  <Spinner size='sm' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : (
+                'Sign In'
+              )}
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Don'tHave an account?</span>
+            <Link to='/sign-up' className='text-blue-500'>
+              Sign Up
+            </Link>
+          </div> 
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
+      </div>
+     </div>
+    </div>
+  );
 }
