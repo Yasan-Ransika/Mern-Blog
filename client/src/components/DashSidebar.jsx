b9a48e3545a8be329bcabf129f1c7d0e63c3dfea
Yasan Ransika <yransika20010216@gmail.com>
2024-09-17 13:19:48 +0530

    add delete uswer

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight, HiDocumentText} from 'react-icons/hi';
+import { HiUser, HiArrowSmRight, HiDocumentText,   HiOutlineUserGroup,} from 'react-icons/hi';
 import { useEffect, useState } from 'react';
 import { Link,useLocation } from 'react-router-dom';
 import { signoutSuccess } from '../redux/user/userSlice';
@@ -58,8 +58,20 @@ export default function DashSidebar() {
               >
                 Posts
               </Sidebar.Item>
-            </Link>    
+            </Link>
+                
             )} 
+                      {currentUser.isAdmin && (
+              <Link to='/dashboard?tab=users'>
+                <Sidebar.Item
+                  active={tab === 'users'}
+                  icon={HiOutlineUserGroup}
+                  as='div'
+                >
+                  Users
+                </Sidebar.Item>
+              </Link>
+          )}
             <Sidebar.Item 
             icon={HiArrowSmRight}
             className='cursor-pointer'
